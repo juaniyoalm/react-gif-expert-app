9c20b16efa2ac22c809a420c5cbacca2ee537fa3
Juan <juaniyoalm@gmail.com>
2021-01-11 09:46:08 +0100

    First commit

---
+++ src/GiftExpertApp.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
+import { GiftGrid } from './components/GiftGrid';
+
+export const GiftExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch']);
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={ setCategories } />
+            <hr/>
+    
+            <ol>
+                { 
+                    categories.map( category => (
+                        <GiftGrid 
+                            key={ category }
+                            category={ category } 
+                        />
+                    ))
+                }
+            </ol>
+        </>
+    )
+}
---
+++ src/components/GiftGrid.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GiftGridItem } from './GiftGridItem';
+
+export const GiftGrid = ({ category }) => {
+
+    const { data:images, loading } = useFetchGifs( category );
+
+    return (
+        <>
+            <h3 className="animate__animated animate__fadeIn" >{ category }</h3>
+
+            { loading && <p className="animate__animated animate__flash" >Loading...</p> }
+
+            <div className="card-grid">
+                {
+                    images.map(image => (
+                        <GiftGridItem key={ image.id } { ...image } />
+                    ))
+                }
+            </div>
+        </>
+    )
+}
